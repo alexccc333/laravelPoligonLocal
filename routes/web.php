c1a15092c78a90cf193b65cd26c59337b2c981f9
alexccc333 <45902813+alexccc333@users.noreply.github.com>
2019-12-09 19:13:26 +0300

    asaas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::resource('rest','RestTestController')->names('restTest');
+
+Route::group(['namespace'=>'Blog','prefix'=>'blog'],function (){
+   Route::resource('posts','PostController')->names('blog.posts');
+//   Route::resource('categories','CategoryController')->only($methods)->names('blog.admin.categories');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+//
